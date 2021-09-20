8c7b8e7d100f16f3b8caa6e40adc2645b022a5bc
ChrisWise07 <90012389+ChrisWise07@users.noreply.github.com>
2021-09-20 19:05:13 +1000

    Add files via upload

---
+++ rrap/rrap_image_for_patch.py
@@ -0,0 +1,70 @@
+import numpy as np
+import cv2
+import sys
+
+
+from torch.functional import Tensor
+from typing import Tuple
+
+sys.path.append("/mnt/c/Users/Chris Wise/Documents/Programming/ZEIT2190/rrap/")
+from rrap_utils import get_rgb_diff, plot_predictions, get_image_as_tensor, append_to_training_progress_file
+from rrap_constants import INITIAL_PREDICTIONS_DIRECTORY
+
+class Image_For_Patch:
+    _path = str
+    _image_num = int
+    _image_as_np_array = np.ndarray
+    _image_size = Tuple[int,int]
+    _image_rbg_diff = Tensor
+    _predictions_boxes = []
+    _centre_point_of_prediction_boxes = []
+    _image_tensor = Tensor
+
+    def __init__(self, path, image_num, object_detector) -> None:
+        self._path = path
+        self._image_num = image_num
+        self._image_as_np_array = self._open_image_as_np_array()
+        self._image_size = (self._image_as_np_array.shape[2:0:-1])
+        self._image_tensor = get_image_as_tensor(path, self._image_size)
+        self._image_rbg_diff = get_rgb_diff(self._image_tensor)
+        self._predictions_boxes = self._generate_predictions_for_image(object_detector)
+        self._centre_point_of_prediction_boxes = self._calculate_centre_point_of_prediction_boxes()
+    
+    def _open_image_as_np_array(self):
+        image = cv2.imread(self._path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = np.stack([image], axis=0).astype(np.float32)
+        return image
+
+    def get_path(self):
+        return self._path
+
+    def get_image_as_np_array(self):
+        return self._image_as_np_array
+
+    def get_image_size(self):
+        return self._image_size
+
+    def get_image_rbg_diff(self):
+        return self._image_rbg_diff
+
+    def _generate_predictions_for_image(self, object_detector):
+        append_to_training_progress_file(f"\n--- Initial Predictions for image {self._image_num} ---")
+        predictions_boxes = plot_predictions(object_detector = object_detector, image = self._image_as_np_array, 
+                                             path = (INITIAL_PREDICTIONS_DIRECTORY + f"image_{self._image_num}"))
+        return predictions_boxes
+
+    def get_predictions_boxes(self):
+        return self._predictions_boxes
+
+    def _calculate_centre_point_of_prediction_boxes(self):
+        top_left_bbox = self._predictions_boxes[0]
+        bottom_right_bbox = self._predictions_boxes[1]
+        return (int((top_left_bbox[0] + bottom_right_bbox[0]) / 2),
+                int((top_left_bbox[1] +  bottom_right_bbox[1]) / 2)) 
+
+    def get_centre_point_of_prediction_boxes(self):
+        return self._centre_point_of_prediction_boxes
+
+    def get_image_num(self):
+        return self._image_num
---
+++ rrap/rrap_main.py
@@ -0,0 +1,91 @@
+import numpy as np
+import os
+import sys
+from datetime import datetime
+
+from art.attacks.evasion import RobustDPatch
+from art.estimators.object_detection import PyTorchFasterRCNN
+
+sys.path.append("/mnt/c/Users/Chris Wise/Documents/Programming/ZEIT2190/rrap/")
+from rrap_utils import save_image, plot_predictions, set_attack_data, record_attack_training_data, append_to_training_progress_file
+from rrap_image_for_patch import Image_For_Patch
+from rrap_constants import IMAGES_DIRECTORY, PATCHES_DIRECTORY, ADVERSARIAL_PREDICTIONS_DIRECTORY, \
+                           ADVERSARIAL_IMAGES_DIRECTORY, TRAINING_DATA_DIRECTORY
+
+#Create ART object detector
+FRCNN = PyTorchFasterRCNN(
+    clip_values=(0, 255), attack_losses=["loss_classifier", "loss_box_reg", "loss_objectness", "loss_rpn_box_reg"]
+)
+
+def get_custom_patch_location(prediction_centre_points, patch_shape):
+    #Here the coordinates are store (y,x) as somewhere in Robust Dpatch they are treated as (y,x) 
+    return ((int(prediction_centre_points[1] - (patch_shape[1]/2)),
+             int(prediction_centre_points[0] - (patch_shape[0]/2))))   
+
+def generate_adversarial_patch(attack, image, training_data_path):
+    append_to_training_progress_file(f"\n\n--- Generating adversarial patch for image {image.get_image_num()} ---")
+    image_copies = np.vstack([image.get_image_as_np_array()]*1)
+
+    for step in range(3):
+        #train adv patch to trick object detector and not to be perceptible
+        patch_adv = attack.generate(x=image_copies, og_image=image, print_nth_num=2, y=None)                        
+        image_adv = attack.apply_patch(x=image.get_image_as_np_array()) 
+
+        #Save adv patch and training data every step
+        save_image(patch_adv, (PATCHES_DIRECTORY + "patch_{}".format(image.get_image_num())))
+        record_attack_training_data(attack, training_data_path)
+        
+        #Decay learning rate every 5 steps
+        if ((step + 1) % 5 == 0):
+            attack.decay_detection_learning_rate()
+            attack.decay_perceptibility_learning_rate()
+
+    return image_adv
+
+def main():
+    startTime = datetime.now()
+    append_to_training_progress_file(str(startTime))
+
+    # Create attack
+    attack = RobustDPatch(estimator=FRCNN, max_iter=2, batch_size=1, verbose=False, 
+                          rotation_weights=(1,1,1,1), brightness_range= (0.4,1.6), decay_rate = 0.95, 
+                          detection_learning_rate=0.1, perceptibility_learning_rate=0.1, 
+                          detection_momentum = 0.9, perceptibility_momentum = 0.9)
+
+    with os.scandir(IMAGES_DIRECTORY) as entries:
+        i = 0
+
+        for entry in entries:
+            append_to_training_progress_file("\n" + "-"*60)
+
+            #Create image object and, print and save predictions
+            image = Image_For_Patch(entry.path, image_num = i, object_detector=FRCNN)
+            attack_training_data_path = (TRAINING_DATA_DIRECTORY + f"training_data_image_{image.get_image_num()}.txt")
+            if os.path.exists(attack_training_data_path):
+                set_attack_data(attack, attack_training_data_path)
+            else:
+                record_attack_training_data(attack, attack_training_data_path)
+            
+            # Customise patch location to centre of prediction box
+            prediction_centre_points = image.get_centre_point_of_prediction_boxes()
+            patch_shape = attack.get_patch_shape()
+            attack.set_patch_location(get_custom_patch_location(prediction_centre_points, patch_shape))
+
+            #create adversarial patch and apply it to orignal image
+            image_adv = generate_adversarial_patch(attack, image, attack_training_data_path)
+    
+            #Print predictions for image with adv patch and save image with adv patch and prediction boxes
+            append_to_training_progress_file(f"\n\n--- Final predictions for image with adversarial patch {i} ---")
+            plot_predictions(object_detector = FRCNN, image = image_adv, path = (ADVERSARIAL_PREDICTIONS_DIRECTORY + f"image_adv_{i}"))
+
+            #Save image with adv patch but with no prediction boxes
+            save_image(image_adv[0], (ADVERSARIAL_IMAGES_DIRECTORY + f"image_adv_{i}"))
+
+            i+=1
+            append_to_training_progress_file("-"*60)
+            append_to_training_progress_file("\n\n")
+
+    append_to_training_progress_file(f"Total time taken: {datetime.now() - startTime}")
+
+if __name__ == "__main__":
+    main()

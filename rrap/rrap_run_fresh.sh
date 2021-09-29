7f764389387bfeb4b7c7178bb1217f78767c7c24
ChrisWise07 <90012389+ChrisWise07@users.noreply.github.com>
2021-09-29 15:20:33 +1000

    Delete rrap directory

--- rrap/rrap_run_fresh.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm ./data/training_progress_file.txt
-rm -rfd ./data/attack_training_data
-mkdir ./data/attack_training_data
-python3 -W ignore ./rrap_main.py
--- rrap/rrap_run_with_previous_training.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm ./data/training_progress_file.txt
-python3 -W ignore ./rrap_main.py
